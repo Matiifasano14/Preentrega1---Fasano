7610cc1d1ec7ea4905fbd924e0deef1d32573621
Matias Fasano <matifasano14@gmail.com>
2025-01-15 15:48:37 -0300

    2da pre entrega

--- scripts/constantes.js
+++ scripts/constantes.js
@@ -1,65 +1,98 @@
-let paquetes = [
+let destinosTuristicos = [
     {
-        destino: "salta",
+        nombre: "salta",
         hotel: "Hotel salta",
-        precio: "$80.000"
+        precio: "$80.000",
+        tipo: "cultura",
     },
     {
-        destino: "salta",
+        nombre: "salta",
         hotel: "Hotel iruya",
-        precio: "$56.000"
-        },
+        precio: "$56.000",
+        tipo: "cultura",
+    },
     {
-        destino: "salta",
+        nombre: "salta",
         hotel: "Sin hospejade",
-        precio: "$30.000"
+        precio: "$30.000",
+        tipo: "cultura",
     },
     {
-        destino: "Bariloche",
+        nombre: "Bariloche",
         hotel: "Bariloche",
-        precio: "$128.000"
+        precio: "$128.000",
+        tipo: "naturaleza",
     },
     {
-        destino: "Bariloche",
+        nombre: "Bariloche",
         hotel: "Melia",
-        precio: "$168.000"
+        precio: "$168.000",
+        tipo: "naturaleza"
     },
     {
-        destino: "Bariloche",
+        nombre: "Bariloche",
         hotel: "Sin hospedaje",
-        precio: "$70.000"
+        precio: "$70.000",
+        tipo: "naturaleza"
     },
     {
-        destino: "Cataratas del Iguazu",
+        nombre: "Cataratas del Iguazu",
         hotel: "Gran Melia Iguazu",
-        precio: "$176.000"
+        precio: "$176.000",
+        tipo: "naturaleza"
     },
     {
-        destino: "Cataratas del iguazu",
+        nombre: "Cataratas del iguazu",
         hotel: "Magestic",
-        precio: "$56.000"
+        precio: "$56.000",
+        tipo: "naturaleza"
     },
     {
-        destino: "Cataratas del iguazu",
+        nombre: "Cataratas del iguazu",
         hotel: "Sin hospedaje",
-        precio: "$30.000"
+        precio: "$30.000",
+        tipo: "naturaleza"
     },
     {
-        destino: "Ushuaia",
+        nombre: "Ushuaia",
         hotel: "Fin del mundo",
-        precio: "$120.000"
+        precio: "$120.000",
+        tipo: "cultura",
     },
     {
-        destino: "Ushuaia",
+        nombre: "Ushuaia",
         hotel: "Snow",
-        precio: "$92.000"
+        precio: "$92.000",
+        tipo: "cultura",
     },
     {
-        destino: "Ushuaia",
+        nombre: "Ushuaia",
         hotel: "Sin hospedaje",
-        precio: "$60.000"
+        precio: "$60.000",
+        tipo: "cultura",
     },
-
 ];
 
-console.table (paquetes);
+
+// Objeto agencia con un método de búsqueda y filtrado
+const agenciaTurismo = {
+    destinos: destinosTuristicos,
+
+    // Método para filtrar destinos por tipo
+    filtrarPorTipo: function (tipo) {
+        return this.destinos.filter(destino => destino.tipo.toLowerCase() === tipo.toLowerCase());
+    },
+
+    // Método para buscar destinos por nombre
+    buscarPorNombre: function (nombre) {
+        return this.destinos.filter(destino => destino.nombre.toLowerCase().includes(nombre.toLowerCase()));
+    },
+};
+
+debugger
+
+// Filtrar destinos de tipo
+console.log(agenciaTurismo.filtrarPorTipo('cultura'));
+
+// Buscar destinos
+console.log(agenciaTurismo.buscarPorNombre("salta"));
--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,31 @@
-
-const nombre = prompt("Ingresa tu nombre")
+const nombre = prompt("Ingresa tu nombre");
 let medioCotizacion;
 
-while(true){
+while (true) {
+    medioCotizacion = prompt(
+        "Bienvenido/a " +
+        nombre +
+        " ingresa 1 si quieres recibir atencion por WhatsApp o ingresa 2 si quieres generar tu cotizacion Online"
+    );
 
-    medioCotizacion = prompt("Bienvenido/a "+ nombre + " ingresa 1 si quieres recibir atencion por WhatsApp o ingresa 2 si quieres generar tu cotizacion Online")
-
-    if (medioCotizacion === "1" || medioCotizacion === "2"){
+    if (medioCotizacion === "1" || medioCotizacion === "2") {
         break;
-    }
-    else{
-        alert("Elige una opcion correcta")
+    } else {
+        alert("Elige una opcion correcta");
     }
 }
 
-while(true){
-    if (medioCotizacion == 1){ 
-        alert("Nuestro numero de WhatsApp es: 3382456789, recuerda que tambien puedes acercarte a nuestras oficinas en Mitre 887 Rosario, Santa fe")
+while (true) {
+    if (medioCotizacion == 1) {
+        alert(
+            "Nuestro numero de WhatsApp es: 3382456789, recuerda que tambien puedes acercarte a nuestras oficinas en Mitre 887 Rosario, Santa fe"
+        );
         break;
-    }
-    else if (medioCotizacion == 2) {
-        alert("A continuacion vamos a generar tu cotizacion online")
+    } else if (medioCotizacion == 2) {
+        alert("A continuacion vamos a generar tu cotizacion online");
         break;
-    }
-
-    else{
-        alert("Elige una opcion correcta")
+    } else {
+        alert("Elige una opcion correcta");
     }
 }
 
@@ -34,31 +34,31 @@ let destino;
 let comboDescuento1;
 let comboDescuento2;
 
-while(true){
-    destino = parseInt(prompt("¿A donde quieres viajar? \n Elige la opcion deseada \n Opcion 1 - Salta $30000 \n Opcion 2 - Bariloche $70000 \n Opcion - 3 Cataratas del Iguazu $30000 \n Opcion - 4 Ushuaia $60000"));
+while (true) {
+    destino = parseInt(
+        prompt(
+            "¿A donde quieres viajar? \n Elige la opcion deseada \n Opcion 1 - Salta $30000 \n Opcion 2 - Bariloche $70000 \n Opcion - 3 Cataratas del Iguazu $30000 \n Opcion - 4 Ushuaia $60000"
+        )
+    );
 
-    if (destino === 1){
-        precioDestino = 30000
+    if (destino === 1) {
+        precioDestino = 30000;
         comboDescuento1 = 1;
         break;
-    }
-    else if (destino === 2){
-        precioDestino = 70000
+    } else if (destino === 2) {
+        precioDestino = 70000;
         comboDescuento1 = 1;
         break;
-    }
-    else if(destino === 3){
-        precioDestino = 30000
+    } else if (destino === 3) {
+        precioDestino = 30000;
         comboDescuento1 = 1;
         break;
-    }
-    else if (destino === 4){
-        precioDestino = 60000
+    } else if (destino === 4) {
+        precioDestino = 60000;
         comboDescuento1 = 1;
         break;
-    }
-    else{
-        alert("Ingrese una opcion correcta")
+    } else {
+        alert("Ingrese una opcion correcta");
     }
 }
 
@@ -68,142 +68,105 @@ let hotelBariloche;
 let hotelIguazu;
 let hotelUshuaia;
 
-while (destino == 1){
+while (destino == 1) {
+    hotelSalta = prompt(
+        "En que hotel te gustaria hospedarte?\n Opcion 1 - Hotel Salta $50000 \n Opcion 2 - Hotel Iruya $40000 \n Opcion 3 - No quiero reservar hotel"
+    );
 
-    hotelSalta = prompt("En que hotel te gustaria hospedarte?\n Opcion 1 - Hotel Salta $50000 \n Opcion 2 - Hotel Iruya $40000 \n Opcion 3 - No quiero reservar hotel")
-
-    if (hotelSalta == 1){
-        precioHotel = 50000
-        comboDescuento2 = 1
+    if (hotelSalta == 1) {
+        precioHotel = 50000;
+        comboDescuento2 = 1;
         break;
-    }
-    else if (hotelSalta == 2){
-        precioHotel = 40000
-        comboDescuento2 = 1
+    } else if (hotelSalta == 2) {
+        precioHotel = 40000;
+        comboDescuento2 = 1;
         break;
-    }
-    else if (hotelSalta == 3){
-        precioHotel = 0
+    } else if (hotelSalta == 3) {
+        precioHotel = 0;
         break;
-    }
-    else{
-        alert("Opcion incorrecta, vuelve a intentarlo")
+    } else {
+        alert("Opcion incorrecta, vuelve a intentarlo");
     }
 }
 
-while (destino == 2){
+while (destino == 2) {
+    hotelBariloche = prompt(
+        "En que hotel te gustaria hospedarte?\n Opcion 1 - Hotel Bariloche $90000 \n Opcion 2 - Gran Melia $140000 \n Opcion 3 - No quiero reservar hotel"
+    );
 
-    hotelBariloche = prompt("En que hotel te gustaria hospedarte?\n Opcion 1 - Hotel Bariloche $90000 \n Opcion 2 - Gran Melia $140000 \n Opcion 3 - No quiero reservar hotel")
-
-    if (hotelBariloche == 1){
-        precioHotel = 90000
-        comboDescuento2 = 1
+    if (hotelBariloche == 1) {
+        precioHotel = 90000;
+        comboDescuento2 = 1;
         break;
-    }
-    else if (hotelBariloche == 2){
-        precioHotel = 140000
-        comboDescuento2 = 1
+    } else if (hotelBariloche == 2) {
+        precioHotel = 140000;
+        comboDescuento2 = 1;
         break;
-    }
-    else if (hotelBariloche == 3){
-        precioHotel = 0
+    } else if (hotelBariloche == 3) {
+        precioHotel = 0;
         break;
-    }
-    else{
-        alert("Opcion incorrecta, vuelve a intentarlo")
+    } else {
+        alert("Opcion incorrecta, vuelve a intentarlo");
     }
 }
 
-while (destino == 3){
+while (destino == 3) {
+    const hotelIguazu = prompt(
+        "En que hotel te gustaria hospedarte?\n Opcion 1 - Gran Melia Iguazu $190000 \n Opcion 2 - Magestic $40000 \n Opcion 3 - No quiero reservar hotel"
+    );
 
-    const hotelIguazu = prompt("En que hotel te gustaria hospedarte?\n Opcion 1 - Gran Melia Iguazu $190000 \n Opcion 2 - Magestic $40000 \n Opcion 3 - No quiero reservar hotel")
-
-    if (hotelIguazu == 1){
-        precioHotel = 190000
-        comboDescuento2 = 1
+    if (hotelIguazu == 1) {
+        precioHotel = 190000;
+        comboDescuento2 = 1;
         break;
-    }
-    else if (hotelIguazu == 2){
-        precioHotel = 40000
-        comboDescuento2 = 1
+    } else if (hotelIguazu == 2) {
+        precioHotel = 40000;
+        comboDescuento2 = 1;
         break;
-    }
-    else if (hotelIguazu == 3){
-        precioHotel = 0
+    } else if (hotelIguazu == 3) {
+        precioHotel = 0;
         break;
-    }
-    else{
-        alert("Opcion incorrecta, vuelve a intentarlo")
+    } else {
+        alert("Opcion incorrecta, vuelve a intentarlo");
     }
 }
 
+while (destino == 4) {
+    hotelUshuaia = prompt(
+        "En que hotel te gustaria hospedarte?\n Opcion 1 - Hotel Fin del Mundo $90000 \n Opcion 2 - Hotel Snow $55000 \n Opcion 3 - No quiero reservar hotel"
+    );
 
-while(destino == 4){ 
-    hotelUshuaia = prompt("En que hotel te gustaria hospedarte?\n Opcion 1 - Hotel Fin del Mundo $90000 \n Opcion 2 - Hotel Snow $55000 \n Opcion 3 - No quiero reservar hotel")
-
-    if (hotelUshuaia == 1){
-        precioHotel = 90000
-        comboDescuento2 = 1
+    if (hotelUshuaia == 1) {
+        precioHotel = 90000;
+        comboDescuento2 = 1;
         break;
-    }
-    else if (hotelUshuaia == 2){
-        precioHotel = 55000
-        comboDescuento2 = 1
+    } else if (hotelUshuaia == 2) {
+        precioHotel = 55000;
+        comboDescuento2 = 1;
         break;
-    }
-    else if (hotelUshuaia == 3){
-        precioHotel = 0
+    } else if (hotelUshuaia == 3) {
+        precioHotel = 0;
         break;
-    }
-    else{
-        alert("Opcion incorrecta, vuelve a intentarlo")
+    } else {
+        alert("Opcion incorrecta, vuelve a intentarlo");
     }
 }
 
 let precioTotal;
 let precioConDescuento;
 
-function sumar (){
+function sumar() {
     precioTotal = precioHotel + precioDestino;
-    alert("El precio total es: "+ precioTotal)
+    alert("El precio total es: " + precioTotal);
 }
 
-sumar()
+sumar();
 
-if ((comboDescuento1 == 1) && (comboDescuento2 == 1)){
-
+if (comboDescuento1 == 1 && comboDescuento2 == 1) {
     precioConDescuento = (precioTotal * 80) / 100;
 
-    alert("Por contratar ambos servicios con nosotros te hacemos un descuento del 20%, el total a pagar te queda en: $"+ precioConDescuento)
-}
-
-class paqueteComprado {
-    ciudad = "";
-    hotel = "";
-    precio = "";
-    constructor (ciudad, hotel, precio){
-        this.ciudad = ciudad;
-        this.hotel = hotel;
-        this.precio = precio;
-    }
-}
-
-const UnPaqueteComprado = new paqueteComprado ("salta", "iruya", 56.000);
-
-console.log ("El paquete que compraste tiene destino a " + this.ciudad + " en el hotel " + this.hotel + " y tiene un costo de " + this.precio)
-
-console.log (UnPaqueteComprado.toString());
-
-class facturaCompra {
-    id ="";
-    cliente = "";
-    paquete = "";
-    detalle = "";
-
-    constructor (id, cliente, paquete, detalle){
-        this.id;
-        this.cliente;
-        this.precio;
-        this.detalle;
-    }
+    alert(
+        "Por contratar ambos servicios con nosotros te hacemos un descuento del 20%, el total a pagar te queda en: $" +
+        precioConDescuento
+    );
 }
